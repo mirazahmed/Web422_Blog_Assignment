b492d97983f295c5abfab5ccfc50c5eb3b98674a
Miraz Ahmed <miraz37@gmail.com>
2020-08-14 21:32:34 -0400

    new routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component' ;
 import { BlogComponent } from './blog/blog.component';
 import { PostComponent } from './post/post.component';
+import {PostsTableComponent} from './posts-table/posts-table.component';
+import {EditPostComponent} from './edit-post/edit-post.component';
+import {NewPostComponent} from './new-post/new-post.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
@@ -10,7 +13,11 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'blog', component: BlogComponent},
+  {path: 'post', component: PostComponent},
   {path: 'post/:id', component: PostComponent},
+  {path: 'admin', component:PostsTableComponent },
+  {path: 'admin/newPost', component: NewPostComponent},
+  {path: 'admin/post/:id', component: EditPostComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent}
 ];
